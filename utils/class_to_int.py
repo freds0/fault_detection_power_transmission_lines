f7207808c44fe2074bb017b6cfbb5411d399276b
Fred <fred.santos.oliveira@gmail.com>
2022-03-10 22:36:44 -0300

    New Feature
    Adding new Classes

--- utils/class_to_int.py
+++ utils/class_to_int.py
@@ -1,17 +1,27 @@
 
 def class_text_to_int(row_label):
-    if row_label == 'esfera_boa':
+    if row_label == 'baliser_ok':
         return 1
-    elif row_label == 'esfera_razoavel':
+    elif row_label == 'baliser_aok':
         return 2
-    elif row_label == 'esfera_ruim':
+    elif row_label == 'baliser_nok':
         return 3
-    elif row_label == 'isolador_ok':
+    elif row_label == 'insulator_ok':
         return 4
-    elif row_label == 'isolador_falha':
+    elif row_label == 'insulator_nok':
         return 5
-    elif row_label == 'ninho':
+    elif row_label == 'bird_nest':
         return 6
+    elif row_label == 'stockbridge_ok':
+        return 7
+    elif row_label == 'stockbridge_nok':
+        return 8
+    elif row_label == 'spacer_ok':
+        return 9
+    elif row_label == 'spacer_nok':
+        return 10
+    elif row_label == 'insulator_unk':
+        return 11
     else:
         print("Erro: " + row_label)
         None
