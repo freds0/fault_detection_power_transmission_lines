d031cf06aedc1f599af6b897bdbfb6ee8cbd3df7
Frederico S. Oliveira <fred_s0@yahoo.com.br>
2022-03-24 11:40:13 -0300

    Parameters Refactoring
    Corrected the checkpoint folder and output folder parameters.

--- test.py
+++ test.py
@@ -7,9 +7,9 @@ from os.path import join
 def execute_model_evaluation():
     parser = argparse.ArgumentParser()
     parser.add_argument('-y', '--yaml', default='config/parameters.yaml', help='Config file YAML format')
-    parser.add_argument('-c', '--checkpoint_dir', help='Folder to load exported checkpoint.')
+    parser.add_argument('-c', '--checkpoint_dir', help='Path to directory where your training job writes checkpoints.')
     parser.add_argument('-p', '--pipeline_file', help='Path to your custom pipeline.config file.')
-    parser.add_argument('-o', '--output_dir', default='./output_inference', help='Folder to save evaluation results.')
+    parser.add_argument('-o', '--output_dir', help='Folder to save evaluation results.')
     parser.add_argument('-n', '--num_workers', default=64, type=int, help='Number of cores that can be used for the evaluation job.')
     args = parser.parse_args()
 
@@ -21,16 +21,16 @@ def execute_model_evaluation():
         print(e)
         exit()
 
-    checkpoint_dir = args.checkpoint_dir if args.checkpoint_dir else join(config['pipeline_config']['checkpoint_save_path'], 'exported')
+    checkpoint_dir = args.checkpoint_dir if args.checkpoint_dir else config['pipeline_config']['checkpoint_save_path']
     pipeline_file = args.pipeline_file if args.pipeline_file else config['pipeline_config']['pipeline_config_filepath']
-    output_dir  = args.output_dir if args.output_dir else join(config['pipeline_config']['checkpoint_save_path'], 'exported')
+    output_dir  = args.output_dir if args.output_dir else config['pipeline_config']['checkpoint_save_path']
     timeout = 3
 
     print("Executing evaluation...")
     subprocess.run(["python3", "/tensorflow/models/research/object_detection/model_main_tf2.py",
         "--pipeline_config_path={}".format(pipeline_file),
-        "--model_dir={}".format(checkpoint_dir),
-        "--checkpoint_dir={}".format(output_dir),
+        "--checkpoint_dir={}".format(checkpoint_dir),                    
+        "--model_dir={}".format(output_dir),
         "--num_workers={}".format(args.num_workers),
         "--eval_timeout={}".format(timeout),
         "--alsologtostderr"
